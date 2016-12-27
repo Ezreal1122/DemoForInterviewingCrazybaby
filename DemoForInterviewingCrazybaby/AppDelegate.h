8787e9e32c4f6988f41f0e92e0fb5ce75641b3e6
吴海文 <iZhenfang@wuhaiwendemac-mini.local>
2016-12-27 14:48:49 +0800

    Initial Commit

---
+++ DemoForInterviewingCrazybaby/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  DemoForInterviewingCrazybaby
+//
+//  Created by 吴海文 on 16/12/27.
+//  Copyright © 2016年 韩. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
